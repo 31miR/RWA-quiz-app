e8c401beec8244627f741d4e7a1906d2ff7a7349
31miR <76799355+31miR@users.noreply.github.com.>
2025-08-03 01:39:42 +0200

    add oneToMany relation to Admin entity with Quiz

--- src/main/java/com/example/kviz/model/Admin.java
+++ src/main/java/com/example/kviz/model/Admin.java
@@ -1,5 +1,8 @@
 package com.example.kviz.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -11,9 +14,27 @@ public class Admin {
     private String username;
     @Column(nullable=false)
     private String password;
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz> quizzes = new ArrayList<>();
 
     public String getUsername() {
         return this.username;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Quiz> getQuizzes() {
+        return this.quizzes;
+    }
+
+    public void setQuizzes(List<Quiz> quizzes) {
+        this.quizzes = quizzes;
     }
 
     public void setUsername(String username) {
