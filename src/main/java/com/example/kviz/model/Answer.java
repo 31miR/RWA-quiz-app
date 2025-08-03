9a651af21050114d126ce01a481ae3463e0164a6
31miR <76799355+31miR@users.noreply.github.com.>
2025-08-03 02:03:43 +0200

    implement Answer entity class

--- src/main/java/com/example/kviz/model/Answer.java
+++ src/main/java/com/example/kviz/model/Answer.java
@@ -1,5 +1,53 @@
 package com.example.kviz.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Answer {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String text;
+    @Column(nullable=false)
+    private boolean isRight;
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isIsRight() {
+        return this.isRight;
+    }
+
+    public boolean getIsRight() {
+        return this.isRight;
+    }
+
+    public void setIsRight(boolean isRight) {
+        this.isRight = isRight;
+    }
+
+    public Question getQuestion() {
+        return this.question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
