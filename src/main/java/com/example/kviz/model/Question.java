cac8974cc3e4804c796fa008b222e7b0252b32fe
31miR <76799355+31miR@users.noreply.github.com.>
2025-08-03 02:00:05 +0200

    implement Question entity class

--- src/main/java/com/example/kviz/model/Question.java
+++ src/main/java/com/example/kviz/model/Question.java
@@ -1,5 +1,82 @@
 package com.example.kviz.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
 public class Question {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String questionText;
+    @Column(nullable=false)
+    private int timeInterval;
+    @Column(nullable=false)
+    private int pointAmount;
+    @Column(nullable=false)
+    private int questionPosition;
+    @ManyToOne
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Answer> answers = new ArrayList<>();
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestionText() {
+        return this.questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public int getTimeInterval() {
+        return this.timeInterval;
+    }
+
+    public void setTimeInterval(int timeInterval) {
+        this.timeInterval = timeInterval;
+    }
+
+    public int getPointAmount() {
+        return this.pointAmount;
+    }
+
+    public void setPointAmount(int pointAmount) {
+        this.pointAmount = pointAmount;
+    }
+
+    public int getQuestionPosition() {
+        return this.questionPosition;
+    }
+
+    public void setQuestionPosition(int questionPosition) {
+        this.questionPosition = questionPosition;
+    }
+
+    public Quiz getQuiz() {
+        return this.quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public List<Answer> getAnswers() {
+        return this.answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
 }
