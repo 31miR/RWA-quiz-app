3a2abd2b962207d15529646613607002ac44d77d
31miR <76799355+31miR@users.noreply.github.com.>
2025-08-07 02:36:22 +0200

    implement whoamiservlet that returns data about logged in admin

---
+++ src/main/java/com/example/kviz/servlet/WhoAmIServlet.java
@@ -0,0 +1,58 @@
+package com.example.kviz.servlet;
+
+import java.io.IOException;
+
+import com.example.kviz.model.Admin;
+import com.example.kviz.service.AdminService;
+import com.google.gson.Gson;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+class AdminRawData {
+    public long id;
+    public String username;
+    public boolean isSuperAdmin;
+}
+
+@WebServlet(name = "WhoAmIServlet", urlPatterns = "/api/whoami")
+public class WhoAmIServlet extends HttpServlet {
+    public final AdminService adminService = new AdminService();
+    public final Gson gson = new Gson();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        HttpSession session = request.getSession(false);
+        
+        if (session == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("{\"message\": \"No session found\"}");
+            return;
+        }
+        
+        if (session.getAttribute("adminId") == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("{\"message\": \"No admin id found in session\"}");
+            return;
+        }
+
+        Admin admin = adminService.findById((Long) session.getAttribute("adminId"));
+
+        if (admin == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("{\"message\": \"No admin with given id found in database\"}");
+            return;
+        }
+
+        AdminRawData adminRaw = new AdminRawData();
+        adminRaw.id = admin.getId();
+        adminRaw.username = admin.getUsername();
+        adminRaw.isSuperAdmin = admin.getIsSuperAdmin();
+
+        response.getWriter().write(gson.toJson(adminRaw));
+    }
+}
